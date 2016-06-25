c320c3a086422e35ed781a3ab1c6f630bec068f9
MorrisB-- <morris.ballenger@yahoo.com>
2016-06-25 11:27:02 -0500

    Finished

--- A_Star_Search/Assets/_Scripts/AStar.cs
+++ A_Star_Search/Assets/_Scripts/AStar.cs
@@ -14,9 +14,30 @@ public class AStar : MonoBehaviour {
 
 		OpenList.Add (StartNode); // Add the start node to the open list
 
-		// Your code here...
-		//
-		//
+		while(OpenList.Count > 0) {
+			Node currentNode = OpenList[0];
+
+			for(int i = 0; i < currentNode.Adjacents.Count; i++) {
+				currentNode.Adjacents[i].G = Vector3.Distance(currentNode.transform.position, currentNode.Adjacents[i].transform.position);
+				currentNode.Adjacents[i].H = Vector3.Distance(currentNode.Adjacents[i].transform.position, TargetNode.transform.position);
+
+				if (!OpenList.Contains(currentNode.Adjacents[i]) && !ClosedList.Contains(currentNode.Adjacents[i])) {
+					OpenList.Add(currentNode.Adjacents[i]);
+					currentNode.Adjacents[i].parent = currentNode;
+				}
+			}
+
+			OpenList.Remove(currentNode);
+			ClosedList.Add(currentNode);
+
+			for (int i = 0; i < OpenList.Count; i++)
+				if (OpenList [i].F < currentNode.F)
+					currentNode = OpenList [i];
+
+			if (currentNode == TargetNode)
+				GetPath(StartNode, TargetNode);
+
+		}
 	}
 
 	void GetPath(Node startNode, Node targetNode){
